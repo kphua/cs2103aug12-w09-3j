5c5c36654006e5dcbe0d700a307137f7ac2d3ad9
Ray <chengwm@gmail.com>
2012-10-07 16:28:12 +0800

    Modified CMD to contain only 2 data: COMMAND_TYPE and Object. COMMAND_TYPE will tell Control what the user command does. Object will contain the data necessary for the relevant command. ie. Object will contain Entry if CMD_TYPE is add, int or String if edit etc.
    Added getter and setter for these 2 data as well.

--- src/CMD.java
+++ src/CMD.java
@@ -1,31 +1,46 @@
-
 class CMD {
 
-	public int type;
+	private COMMAND_TYPE commandType;
+	private Object data;
+
+	public CMD(String command, Object data) {
+		setCommandType(determineCommandType(command));
+		this.setData(data);
+	}
 	
 	enum COMMAND_TYPE {
-		ADD, DELETE, UNDO, DISPLAY, EDIT
+		ADD, DELETE, UNDO, DISPLAY, EDIT, ERROR
 	};
-	
-	String commandString = getFirstWord(userCommand);
-	
-	COMMAND_TYPE commandType = determineCommandType(commandString);
-	
-	public COMMAND_TYPE determineCommandType(String commandString) {
+
+	private COMMAND_TYPE determineCommandType(String commandString) {
 		if (commandString.equalsIgnoreCase("add")) {
 			return COMMAND_TYPE.ADD;
-		}
-		else if (commandString.equalsIgnoreCase("delete")) {
+		} else if (commandString.equalsIgnoreCase("delete")) {
 			return COMMAND_TYPE.DELETE;
-		}
-		else if (commandString.equalsIgnoreCase("display")) {
+		} else if (commandString.equalsIgnoreCase("display")) {
 			return COMMAND_TYPE.DISPLAY;
-		}
-		else if (commandString.equalsIgnoreCase("undo")) {
+		} else if (commandString.equalsIgnoreCase("undo")) {
 			return COMMAND_TYPE.UNDO;
-		}
-		else if (commandString.equalsIgnoreCase("edit")) {
+		} else if (commandString.equalsIgnoreCase("edit")) {
 			return COMMAND_TYPE.EDIT;
-		}
-	
+		} else
+			return COMMAND_TYPE.ERROR;
+
+	}
+
+	public COMMAND_TYPE getCommandType() {
+		return commandType;
+	}
+
+	public void setCommandType(COMMAND_TYPE commandType) {
+		this.commandType = commandType;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
 }
