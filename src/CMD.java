e2dd3786663093c5b76910b893a19a014a36f436
Ray <chengwm@gmail.com>
2012-10-07 17:16:38 +0800

    CMD.java: Shifted COMMAND_TYPE functionality to Processor.java
    Processor.java:
    Added COMMAND_TYPE functionality.
    Setup template for different treatment of different COMMAND_TYPEs.

--- src/CMD.java
+++ src/CMD.java
@@ -1,38 +1,19 @@
-class CMD {
 
-	private COMMAND_TYPE commandType;
+class CMD {
+	
+	private Processor.COMMAND_TYPE commandType;
 	private Object data;
 
-	public CMD(String command, Object data) {
-		setCommandType(determineCommandType(command));
+	public CMD(Processor.COMMAND_TYPE command, Object data) {
+		commandType = command;
 		this.setData(data);
 	}
-	
-	enum COMMAND_TYPE {
-		ADD, DELETE, UNDO, DISPLAY, EDIT, ERROR
-	};
 
-	private COMMAND_TYPE determineCommandType(String commandString) {
-		if (commandString.equalsIgnoreCase("add")) {
-			return COMMAND_TYPE.ADD;
-		} else if (commandString.equalsIgnoreCase("delete")) {
-			return COMMAND_TYPE.DELETE;
-		} else if (commandString.equalsIgnoreCase("display")) {
-			return COMMAND_TYPE.DISPLAY;
-		} else if (commandString.equalsIgnoreCase("undo")) {
-			return COMMAND_TYPE.UNDO;
-		} else if (commandString.equalsIgnoreCase("edit")) {
-			return COMMAND_TYPE.EDIT;
-		} else
-			return COMMAND_TYPE.ERROR;
-
-	}
-
-	public COMMAND_TYPE getCommandType() {
+	public Processor.COMMAND_TYPE getCommandType() {
 		return commandType;
 	}
 
-	public void setCommandType(COMMAND_TYPE commandType) {
+	public void setCommandType(Processor.COMMAND_TYPE commandType) {
 		this.commandType = commandType;
 	}
 
--- src/Processor.java
+++ src/Processor.java
@@ -1,20 +1,61 @@
+import java.util.Hashtable;
 
 public class Processor {
-
-	//enumeration of commands
-	//tree/list of reserved words to translate into commands		
 	
-	//Reader to load file with reserved words into tree/list
+	private Hashtable <String, String> reservedWords;
+	private static final String ERROR_MSG_INVALID_INPUT = "Invalid Input.";
 	
-	public CMD[] translateToCMD(String userInput) {
-		// TODO Auto-generated method stub
+	
+	public Processor(){
+		//load txt file with reserved words into Hashtable
 		
-		//breaks the userInput down into its commands and data
-			//repackage n return
+	}
+	
+	
+	public CMD translateToCMD(String userInput) {
 		
-		// test comment -F
+		String[] temp = userInput.split(" ");
+		if(!reservedWords.contains(temp[0])) {
+			
+			return (new CMD(COMMAND_TYPE.ERROR, ERROR_MSG_INVALID_INPUT));
+		}
+		else{
+			String cmd = reservedWords.get(temp[0]);
+			COMMAND_TYPE userCMD = determineCommandType(cmd);
+			
+			switch(userCMD){
+				case ADD:
+					break;
+//				case EDIT:
+//				case REMOVE:
+				case UNDO: return new CMD(userCMD, null);
+				case QUIT: return new CMD(userCMD, null);	
+				default:
+					break;
+			}
+		}
 		
-		return null;
+		return null;		//to be removed
 	}
 
+	enum COMMAND_TYPE {
+		ADD, DELETE, UNDO, DISPLAY, EDIT, QUIT, ERROR
+	};
+	
+	private COMMAND_TYPE determineCommandType(String commandString) {
+		if (commandString.equalsIgnoreCase(".add")) {
+			return COMMAND_TYPE.ADD;
+		} else if (commandString.equalsIgnoreCase(".delete")) {
+			return COMMAND_TYPE.DELETE;
+		} else if (commandString.equalsIgnoreCase(".display")) {
+			return COMMAND_TYPE.DISPLAY;
+		} else if (commandString.equalsIgnoreCase(".undo")) {
+			return COMMAND_TYPE.UNDO;
+		} else if (commandString.equalsIgnoreCase(".edit")) {
+			return COMMAND_TYPE.EDIT;
+		} else if (commandString.equalsIgnoreCase(".quit")) {
+			return COMMAND_TYPE.QUIT;
+		} else
+			return COMMAND_TYPE.ERROR;
+	}
 }
