032014b78622902ce1f024be787fee9a6d403132
Ray <chengwm@gmail.com>
2012-10-09 16:49:35 +0800

    implemented quit and help functionality

--- src/Control.java
+++ src/Control.java
@@ -17,25 +17,37 @@ class Control {
 	public String performAction(String userInput) {
 		// TODO Auto-generated method stub
 
-		CMD commands = processor.translateToCMD(userInput);
+		CMD command = processor.translateToCMD(userInput);
 
 		// CMD will be a Pair of enumeration, data
 
-		switch (commands.getCommandType()) {
+		switch (command.getCommandType()) {
 		// need to store previous version everytime in case of undo action
 		case ADD:
-			return processor.add(commands.getData());
+			storage.addEntry(command.getData());
+			//modify undo
+			//activate edit mode.
+			//ensure save
+			return processor.add(command.getData());
 		case REMOVE:
+			//if there is nothing in command.getData, get active list over to tempList, 
+						//then ask user what he want to remove
+			//if command.data contains hashtag, do a search for the hashtag, port to tempList, 
+						//then ask user what he want to remove
+			//if there is something in the Storage's tempList 
 			storage.display();
-			return processor.remove(commands.getData());
+			return processor.remove(command.getData());
 		case UNDO:
-			return storage.undo(commands.getData());
+			return storage.undo(command.getData());
 		case DISPLAY:
-			return storage.display(commands.getData());
+			return storage.display(command.getData());
 		case EDIT:
-			return processor.edit(commands.getData());
+			return processor.edit(command.getData());
 		case QUIT:
-			return storage.quit(commands.getData());
+			return "exit";
+		case HELP:
+			return "help";
+			
 		default:
 			return MSG_ERROR;
 		}
--- src/FingerTips.java
+++ src/FingerTips.java
@@ -9,23 +9,28 @@ class FingerTips {
 		Control control = new Control();
 
 		// print welcome/help msg etc.
-		showToUser("Welcome To FingerTips!");
-		showToUser("Commands:");
-		showToUser(".a/.add to add, .r/.remove to remove, .e/.edit to edit");
-		showToUser(".u/.undo to undo, .d/.display to display, .q/.quit to quit");
-		showToUser(".h/.help to display help for FingerTips");
-		System.out.print("Command: ");
-
+		System.out.println("Welcome To FingerTips!");
+		System.out.println("Enter \".help\" for further usage instructions.");
+		
 		while (true) {
+			System.out.print("Command: ");
 			userInput = sc.nextLine();
 			actionMSG = control.performAction(userInput);
+			if(actionMSG.equals("exit")) break;
+			else if(actionMSG.equals("help")) help();
 			System.out.println(actionMSG);
 
-			// if userInput.equals("exit") break;
 		}
-
+		
+		sc.close();
 	}
 	
+	private static void help() {
+		System.out.println(".a/.add to add, .r/.remove to remove, .e/.edit to edit");
+		System.out.println(".u/.undo to undo, .d/.display to display, .q/.quit to quit");
+		System.out.println(".h/.help to display help for FingerTips");
+	}
+
 	public static void showToUser(String text) {
 		System.out.println(text);
 	}
