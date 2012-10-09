ee09b19389680859923c63cb6be4dad6266e59b3
Ray <chengwm@gmail.com>
2012-10-09 17:31:09 +0800

    Help functionality for opening page is up. Quit function is working as well.
    Added document for reserved words both for normal mode and edit mode.
    
    Now handles exception where core files are missing.

--- src/Control.java
+++ src/Control.java
@@ -23,26 +23,26 @@ class Control {
 
 		switch (command.getCommandType()) {
 		// need to store previous version everytime in case of undo action
-		case ADD:
-			storage.addEntry(command.getData());
+//		case ADD:
+//			storage.addEntry(command.getData());
 			//modify undo
 			//activate edit mode.
 			//ensure save
-			return processor.add(command.getData());
-		case REMOVE:
+//			return processor.add(command.getData());
+//		case REMOVE:
 			//if there is nothing in command.getData, get active list over to tempList, 
 						//then ask user what he want to remove
 			//if command.data contains hashtag, do a search for the hashtag, port to tempList, 
 						//then ask user what he want to remove
 			//if there is something in the Storage's tempList 
-			storage.display();
-			return processor.remove(command.getData());
-		case UNDO:
-			return storage.undo(command.getData());
-		case DISPLAY:
-			return storage.display(command.getData());
-		case EDIT:
-			return processor.edit(command.getData());
+//			storage.display();
+//			return processor.remove(command.getData());
+//		case UNDO:
+//			return storage.undo(command.getData());
+//		case DISPLAY:
+//			return storage.display(command.getData());
+//		case EDIT:
+//			return processor.edit(command.getData());
 		case QUIT:
 			return "exit";
 		case HELP:
@@ -52,6 +52,5 @@ class Control {
 			return MSG_ERROR;
 		}
 		
-		return null;
 	}
 }
--- src/FingerTips.java
+++ src/FingerTips.java
@@ -6,19 +6,32 @@ class FingerTips {
 	public static void main(String args[]) throws FileNotFoundException {
 		String actionMSG, userInput;
 		Scanner sc = new Scanner(System.in);
-		Control control = new Control();
-
+		boolean cont = true;
+		Control control = null;
+		try{
+			control = new Control();
+		}
+		catch(FileNotFoundException e){
+			System.out.println("Fatal Error. Critical files are missing.");
+			System.out.println("Re-install Program or contact your service provider.");
+			System.out.println("Program will now terminate.");
+			System.out.println("Press Enter to continue.");
+			sc.nextLine();
+			System.exit(0);
+		}
+		
 		// print welcome/help msg etc.
 		System.out.println("Welcome To FingerTips!");
-		System.out.println("Enter \".help\" for further usage instructions.");
+		System.out.println("Enter .help for further usage instructions.");
 		
-		while (true) {
+		while (cont) {
 			System.out.print("Command: ");
 			userInput = sc.nextLine();
 			actionMSG = control.performAction(userInput);
 			if(actionMSG.equals("exit")) break;
 			else if(actionMSG.equals("help")) help();
-			System.out.println(actionMSG);
+			else
+				System.out.println(actionMSG);
 
 		}
 		
