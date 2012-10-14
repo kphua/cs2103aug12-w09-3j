802d5edadade79b41db0c6b60e847a628b478574
Ray <chengwm@gmail.com>
2012-10-14 14:17:43 +0800

    Implemented DONE Changed commands to work without .

--- src/Processor.java
+++ src/Processor.java
@@ -38,67 +38,89 @@ public class Processor {
 	public CMD translateToCMD(String userInput) {
 
 		String[] temp = userInput.split(" ");
+
+		//check if input is valid
+		if(temp.length==0){ 
+			return new CMD(COMMAND_TYPE.ERROR, " ");
+		}
+
+		//check if first word is a command
+		temp[0] = temp[0].toLowerCase();
 		if(!reservedWordsConverter.contains(temp[0])) {
 			return (new CMD(COMMAND_TYPE.ERROR, ERROR_MSG_INVALID_INPUT));
 		}
-		else{
-			String cmd = reservedWordsConverter.get(temp[0]);
-			COMMAND_TYPE userCMD = determineCommandType(cmd);
 
-			switch(userCMD){
-			// Add a command to end edit mode			
-			/*				case ADD:
+		String cmd = reservedWordsConverter.get(temp[0]);
+		COMMAND_TYPE userCMD = determineCommandType(cmd);
+
+		switch(userCMD){
+		// Add a command to end edit mode			
+		/*				
+					case ADD:
 					create entry
 					return newCMD(userCMD, entry);
-			 *note case when user only enters .add alone
-			 */
-
-			case DISPLAY:
-				if(temp.length == 1) return new CMD(userCMD, null);
-				else{
-					if(temp.length > 2){
-						String searchCriteria = mergeString(temp, 1, temp.length);
-						return new CMD(userCMD, searchCriteria);
-					} else {
-						if(isInteger(temp[1])){
-							Integer i = Integer.parseInt(temp[1]);
-							return new CMD(userCMD, i);
-						}
-						return new CMD(userCMD, temp[1]);
-					}
-				}
-			
-			case EDIT:
-				if(temp.length == 1) return new CMD(userCMD, null);
-				else {
-					
-					if(isInteger(temp[1])){
+		 *note case when user only enters .add alone
+		 */
+		
+		case DONE:
+			if(temp.length > 1 && isInteger(temp[2])){
+				Integer i = Integer.parseInt(temp[2]);
+				return new CMD(userCMD, i);
+			}
+			else return new CMD(COMMAND_TYPE.ERROR, ERROR_MSG_INVALID_INPUT);
+		
+		case DISPLAY:
+			if(temp.length == 1) {								//display <nothing>
+				return new CMD(userCMD, null);
+			}
+			else{
+				if(temp.length > 2){							//display <long string>
+					String searchCriteria = mergeString(temp, 1, temp.length);
+					return new CMD(userCMD, searchCriteria);
+				} else {
+					if(isInteger(temp[1])){						//display <number>
 						Integer i = Integer.parseInt(temp[1]);
 						return new CMD(userCMD, i);
 					}
-					else{
-						return new CMD(userCMD, null);
-					}
+					return new CMD(userCMD, temp[1]);			//display <hash/search criteria>
 				}
+			}
 
-			case REMOVE: 
-				if(temp.length==1) return new CMD(userCMD, null);
-				if(isInteger(temp[1])){
+		case EDIT:
+			if(temp.length == 1) {								//edit <nothing>
+				return new CMD(userCMD, null);					
+			}
+			else {
+
+				if(isInteger(temp[1])){							//edit <number>
 					Integer i = Integer.parseInt(temp[1]);
 					return new CMD(userCMD, i);
 				}
 				else{
-					return new CMD(userCMD, temp[1]);
+					return new CMD(userCMD, null);				//input is invalid, but assumes user wants to edit something
 				}
-			case UNDO: return new CMD(userCMD, null);
-			case QUIT: return new CMD(userCMD, null);	
-			default:
-				return new CMD(userCMD, ERROR_MSG_INVALID_INPUT);
 			}
+
+		case REMOVE: 
+			if(temp.length==1) {								//remove <nothing>
+				return new CMD(userCMD, null);
+			}
+			if(isInteger(temp[1])){								//remove <number>
+				Integer i = Integer.parseInt(temp[1]);
+				return new CMD(userCMD, i);
+			}
+			else{
+				return new CMD(userCMD, temp[1]);				//remove <hash>
+			}
+		case UNDO: return new CMD(userCMD, null);
+		case QUIT: return new CMD(userCMD, null);	
+		default:
+			return new CMD(userCMD, ERROR_MSG_INVALID_INPUT);
 		}
+
 	}
 
-
+	//checks if a string can be converted into an integer
 	private boolean isInteger(String string) {
 		try{
 			Integer i = Integer.parseInt(string);
@@ -108,7 +130,8 @@ public class Processor {
 			return false;
 		}
 	}
-
+	
+	
 	private String mergeString(String[] temp, int i, int length) {
 		String mergedString = temp[i];
 		for(i+=1; i<length; i++){
@@ -118,24 +141,26 @@ public class Processor {
 	}
 
 	enum COMMAND_TYPE {
-		ADD, REMOVE, UNDO, DISPLAY, EDIT, QUIT, ERROR, HELP
+		ADD, REMOVE, UNDO, DISPLAY, EDIT, QUIT, ERROR, HELP, DONE
 	};
 
 	private COMMAND_TYPE determineCommandType(String commandString) {
-		if (commandString.equalsIgnoreCase(".add")) {
+		if (commandString.equalsIgnoreCase("add")) {
 			return COMMAND_TYPE.ADD;
-		} else if (commandString.equalsIgnoreCase(".remove")) {
+		} else if (commandString.equalsIgnoreCase("remove")) {
 			return COMMAND_TYPE.REMOVE;
-		} else if (commandString.equalsIgnoreCase(".display")) {
+		} else if (commandString.equalsIgnoreCase("display")) {
 			return COMMAND_TYPE.DISPLAY;
-		} else if (commandString.equalsIgnoreCase(".undo")) {
+		} else if (commandString.equalsIgnoreCase("undo")) {
 			return COMMAND_TYPE.UNDO;
-		} else if (commandString.equalsIgnoreCase(".edit")) {
+		} else if (commandString.equalsIgnoreCase("edit")) {
 			return COMMAND_TYPE.EDIT;
-		} else if (commandString.equalsIgnoreCase(".quit")) {
+		} else if (commandString.equalsIgnoreCase("quit")) {
 			return COMMAND_TYPE.QUIT;
-		} else if (commandString.equalsIgnoreCase(".help")) {
+		} else if (commandString.equalsIgnoreCase("help")) {
 			return COMMAND_TYPE.HELP;	
+		} else if (commandString.equalsIgnoreCase("done")) {
+			return COMMAND_TYPE.DONE;
 		} else
 			return COMMAND_TYPE.ERROR;
 	}
