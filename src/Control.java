ca683b40f0d2d15aab5091bae6663f751687691c
Ray <chengwm@gmail.com>
2012-11-07 21:34:43 +0800

    Bugfix

---
+++ src/Control.java
@@ -0,0 +1,400 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Vector;
+import java.util.logging.Logger;
+
+class Control {
+	private static final String MSG_ERROR_INVALID_INDEX = "Invalid input. Enter a valid index number.";
+	private static final String MSG_ERROR_EDIT_EMPTY_ACTIVELIST = "There is nothing to edit.";
+	private static Control control;
+	private Processor processor;
+	private Storage storage;
+//	private boolean edit, newList; // modes
+	private LinkedList<CMD> undo, redo;
+	private Entry editHolder;
+	private static final Logger logger = Logger.getLogger(Control.class.getName());
+
+	private Control() {
+		logger.setParent(UI.getLoggingParent());
+		logger.info("Initialising Control.");
+		
+		storage = Storage.getInstance();
+		processor = new Processor();
+		undo = new LinkedList<CMD>();
+		redo = new LinkedList<CMD>();
+		
+		logger.info("Control Initialised.");
+	}
+	
+	public static Control getInstance() {
+		if (control == null) {
+			control = new Control();
+		}
+		return control;
+	}
+
+	public CMD performAction(String userInput) {
+
+		CMD command = processor.translateToCMD(userInput);
+
+		return carryOutCMD(command);
+	}
+
+	/**
+	 * @param command
+	 * @return
+	 */
+	private CMD carryOutCMD(CMD command) {
+		switch (command.getCommandType()) {
+		// need to store previous version every time in case of undo action
+		case ADD:			return add(command);
+		case REMOVE:		return remove(command);
+		case CLEAR:			return clear(command);
+		case UNDO:			return undo(command);
+		case REDO:			return redo(command);			
+		case DISPLAY:		return display(command);
+		case EDIT:			return edit(command);
+		case DONE:			return done(command);
+		default:			return command;
+//		case QUIT: case HELP:
+//			return command;
+		
+		}
+	}
+
+	/**
+	 * @param command
+	 * @return
+	 */
+	private CMD done(CMD command) {
+		
+		if(isInteger(command.getData())){
+			Integer i = (Integer) command.getData();
+			if(i > storage.getActiveEntries().size() || i < 1) {
+				command.setCommandType(Processor.COMMAND_TYPE.ERROR);
+				command.setData("Invalid index.");
+			} else {
+				Entry e = storage.updateCompletedEntry(i);
+				command.setData(e);
+				undo.push(command);
+				storage.save(true, true);
+			}
+		} else {
+			Entry e = (Entry) command.getData();
+			storage.getActiveEntries().add(e);
+			storage.getArchiveEntries().remove(e);
+		}
+		
+		return command;
+	}
+
+	/**
+	 * @param command
+	 * @return
+	 */
+	private CMD edit(CMD command) {
+		//check if number given by edit <number> is valid
+		//if it is valid, load the entry into tempHold, then convert to add's edit
+		//if not convert to edit <nothing>
+		
+		if(storage.getActiveEntries().isEmpty()) {
+			command.setCommandType(Processor.COMMAND_TYPE.ERROR);
+			command.setData(MSG_ERROR_EDIT_EMPTY_ACTIVELIST);
+			return command;
+		}
+		
+		if((int)command.getData() > storage.getActiveEntries().size() || (int)command.getData()<1) {	
+			command.setCommandType(Processor.COMMAND_TYPE.ERROR);
+			command.setData(MSG_ERROR_INVALID_INDEX);
+		}
+		else{
+			editHolder = storage.getActiveEntries().get((int)command.getData()-1);
+			command.setData(null);
+		}
+		
+		return command;
+	}
+
+	/**
+	 * @param command
+	 * @return
+	 */
+	private CMD display(CMD command) {
+		if (command.getData() == null) {
+			command.setData((Vector<Entry>)storage.display());
+		}
+		else {
+				
+			// if the data is integer to specify index
+			if(isInteger(command.getData())){						
+				Integer index = (Integer) command.getData();
+				command.setData(storage.displayIndex(index));
+			}
+			// if the data is string to be searched
+			else {
+				String keyword = (String) command.getData();
+				command.setData(storage.displayKeyword(keyword));
+			}
+		}
+		
+		return command;
+	}
+
+
+	/**
+	 * @param command
+	 * @return
+	 */
+	private CMD clear(CMD command) {
+		command.setData(storage.getActiveEntries());
+		undo.push(command);
+		storage.clearActive();
+		storage.save(true, false);
+		return command;
+	}
+
+	/**
+	 * @param command
+	 * @return
+	 */
+	private CMD remove(CMD command) {
+		//if there is nothing in command.getData, get active list over to tempList, 
+					//then ask user what he want to remove
+		//if command.data contains hashtag, do a search for the hashtag, port to tempList, 
+					//then ask user what he want to remove
+		//if there is something in the Storage's tempList 
+		
+		if(isInteger(command.getData())) {
+			Integer i = (Integer) command.getData(); 
+			if(i<1 || i>storage.getDisplayEntries().size()){
+				command.setCommandType(Processor.COMMAND_TYPE.ERROR);
+				command.setData("Invalid Index.\n");
+				return command;
+			}
+
+			command.setData(storage.getDisplayEntries().get(i-1));
+			undo.push(command);
+			storage.removeEntry(i);
+			storage.save(true, false);
+		}
+		return command;
+	}
+
+	/**
+	 * @param command
+	 * @return
+	 */
+	private CMD add(CMD command) {
+		if(command.getData()!=null){
+			editHolder = (Entry) command.getData();
+			storage.addEntry(editHolder);
+			storage.save(true, false);
+			command.setData(editHolder);
+			undo.push(command);
+		}
+					
+		return command;
+	}
+
+	/**
+	 * @param command
+	 * @return
+	 */
+	
+	private CMD redo(CMD command) {
+		if(redo.isEmpty()) {
+			command.setData("There are no further redo-s.\n");
+			return command; 
+		}
+		
+		CMD action = redo.pop();
+		undo.push(action);
+		
+		switch(action.getCommandType()){
+		case EDIT:
+			Entry original = (Entry)action.getData();
+			action.setData(storage.removeEntry(original.getID()));
+			storage.addEntry(original);
+			
+			break;
+			default:
+				carryOutCMD(action);
+			
+		}
+		if(action.getCommandType()==Processor.COMMAND_TYPE.DONE){
+			storage.save(true, true);
+		}
+		else {
+			storage.save(true, false);
+		}
+		
+		command.setData("Redo completed\n");
+		return command;
+	}
+
+	/**
+	 * @param command
+	 * @return
+	 */
+	
+	private CMD undo(CMD command) {
+		if(undo.isEmpty()) {
+			command.setData("There are no further undo-s.\n");
+			return command; 
+		}
+		
+		CMD action = undo.pop();
+		redo.push(action);
+		
+		switch(action.getCommandType()){
+		case ADD: 
+			storage.removeEntry((Entry)action.getData());
+			break;		//amend storage remove function to search for objects and not indexes before implement
+		case REMOVE: 
+			storage.addEntry((Entry)action.getData());
+			break;
+		case CLEAR: 
+			storage.setActiveEntries((Vector<Entry>) action.getData());
+			storage.setDisplayEntries(storage.getActiveEntries());
+			break;
+		case EDIT: 
+			Entry original = (Entry)action.getData();
+			action.setData(storage.removeEntry(original.getID()));
+			storage.addEntry(original);
+			
+			break;		//each entry needs a unique identity for this to work...
+		case DONE:
+			storage.undoDoneAction((Entry)action.getData());
+			break;		//storage function to move it back...Entry is given under data of Undo
+			default: 
+				logger.severe("Unexpected CMD action recorded.");
+				assert false;	
+		}
+		
+		if(action.getCommandType()==Processor.COMMAND_TYPE.DONE){
+			storage.save(true, true);
+		}
+		else {
+			storage.save(true, false);
+		}
+		
+		command.setData("Undo completed.\n");
+		return command;
+		
+	}
+
+
+	//checks if a string can be converted into an integer
+	private boolean isInteger(Object object) {
+		try{
+			Integer.parseInt((String) object);
+			return true;
+		}
+		catch(NumberFormatException e){
+			return false;
+		}
+		catch(ClassCastException c){
+			return true;
+		}
+	}
+
+	public Entry getTempHold() {
+		return editHolder;
+	}
+
+	public void setEditHolder(Entry tempHold) {
+		this.editHolder = tempHold;
+	}
+
+	public Storage getStorage() {
+		return storage;
+	}
+
+	public void setStorage(Storage storage) {
+		this.storage = storage;
+	}
+	
+	public Processor getProcessor() {
+		return processor;
+	}
+
+	public void setProcessor(Processor processor) {
+		this.processor = processor;
+	}
+
+	public String[] processEditMode(String userInput) {
+		String[] cmd = processor.determineCmdEditMode(userInput);
+		CMD clone = new CMD(Processor.COMMAND_TYPE.EDIT, new Entry(editHolder));
+		
+		if(cmd.length > 1 && cmd[1] != null) cmd[1] = cmd[1].trim();
+		if(cmd[0].equals("description")){	
+			if(cmd[1]!=null && cmd[1].length()!=0)
+				editHolder.setDesc(cmd[1]);
+		} else if(cmd[0].equals("duedate")){
+			if(processor.isDate(cmd[1])){			//to be amended
+				editHolder.iniDDate();
+				editHolder.setDateCal(cmd[1]);
+			}
+			else cmd = new String[] {"Error", "Invalid entry for date."};
+		} else if(cmd[0].equals("starttime")){
+			if(cmd[1].endsWith("am") || cmd[1].endsWith("pm"))
+				editHolder.setStart(cmd[1]);
+			else cmd = new String[] {"Error", "Invalid time entry."};
+		} else if(cmd[0].equals("endtime")){
+			if(cmd[1].endsWith("am") || cmd[1].endsWith("pm"))
+				editHolder.setEnd(cmd[1]);
+			else cmd = new String[] {"Error", "Invalid time entry."};
+		} else if(cmd[0].equals("hash")){
+			if(cmd[1].startsWith("#"))
+				editHolder.setTagDesc(cmd[1]);
+			else cmd = new String[] {"Error", "Not a hashtag"};
+		} else if(cmd[0].equals("display")){
+			cmd = new String[]{cmd[0], null};
+			cmd[1] = printEntry(editHolder);
+		} else if(cmd[0].equals("priority")){
+			boolean restrictedWords = cmd[1].equalsIgnoreCase("high") || 
+					cmd[1].equalsIgnoreCase("medium") || cmd[1].equalsIgnoreCase("low");
+			if(restrictedWords){
+				editHolder.setPriority(cmd[1].toUpperCase());
+				//sort
+			}
+			else cmd = new String[] {"Error", "Not a priority"};
+		} else if(cmd[0].equals("venue")){
+			if(cmd[1].startsWith("@"))
+				editHolder.setVenue(cmd[1]);
+			else cmd = new String[] {"Error", "Invalid format for location."};
+		} else if(cmd[0].equals("help")){
+			return cmd;
+		} else if(cmd[0].equals("end")){
+			return cmd;
+		} else {
+			cmd = new String[] {"Error", "Invalid edit field."};
+		}
+		
+		
+		if(!cmd[0].equals("Error")) {
+			undo.push(clone);
+			Collections.sort(storage.getActiveEntries());
+			storage.save(true, false);
+		}
+		
+		return cmd;
+	}
+	
+	private String printEntry(Entry entry) {
+		String lineToPrint = entry.getDesc();
+		if (entry.getStart() != null) 
+			lineToPrint = lineToPrint.concat(" at " + entry.getStart());
+		if (entry.getEnd() != null) 
+			lineToPrint = lineToPrint.concat(" to " + entry.getEnd());
+		if (entry.getDate() != null) 
+			lineToPrint = lineToPrint.concat(" on " + entry.getDate());
+		if (entry.getVenue() != null) 
+			lineToPrint = lineToPrint.concat(" " + entry.getVenue());
+		if (entry.getPriority() != null) 
+			lineToPrint = lineToPrint.concat(" " + entry.getPriority());
+		if (entry.getTagDesc() != null) 
+			lineToPrint = lineToPrint.concat(" " + entry.getTagDesc());
+		return lineToPrint;
+	}
+}
---
+++ test/testProcessor.java
@@ -0,0 +1,166 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class testProcessor {
+	
+	Processor p = new Processor();
+
+/*
+	@Test
+	public void testProcessor() {
+		fail("Not yet implemented");
+	}
+*/
+
+	@Test
+	public void testTranslateToCMD() {
+		
+		String[] input = new String[19];
+		String[] actualOutput = new String[19];
+		String[] expectedOutput = new String[19];
+		
+		input[0] = "  ";		// returns ERROR & Invalid Input & Input should follow a "<command> <data>" format
+		input[1] = "testing"; 	// returns ERROR & Invalid Input & Input should follow a "<command> <data>" format
+		// returns ADD & Test for add from 1pm to 2pm on 1/1/2012
+		input[2] = "add \"Test for add\" 1pm 2pm 1/1/2012";
+		input[3] = "clear";		// returns CLEAR & null
+		input[4] = "done";		// returns ERROR & Invalid Input & Input should follow a "<command> <data>" format
+		input[5] = "done 2";	// returns DONE & 2
+		input[6] = "display";	// returns DISPLAY & null
+		input[7] = "display 3";	// returns DISPLAY & 3
+		input[8] = "display testing display";	// returns DISPLAY & testing display
+		input[9] = "display #hash";	// returns DISPLAY & #hash
+		input[10] = "edit";	// returns EDIT & null
+		input[11] = "edit 3";	// returns EDIT & 3
+		input[12] = "edit nonsense";	// returns EDIT & null
+		input[13] = "remove";	// returns REMOVE & null
+		input[14] = "remove 3";	// returns REMOVE & 3
+		input[15] = "remove hello";	// returns REMOVE & hello
+		input[16] = "undo";	// returns UNDO & null
+		input[17] = "quit";	// returns QUIT & null
+		input[18] = "help";	// returns HELP & null
+		
+		// set expectedOutput results
+		expectedOutput[0] = "ERROR Invalid Input. Input should follow a \"<command> <data>\" format";
+		expectedOutput[1] = "ERROR Invalid Input. Input should follow a \"<command> <data>\" format";
+		expectedOutput[2] = "ADD Test for add from 1pm to 2pm on 1/1/2012 ";
+		expectedOutput[3] = "CLEAR null";
+		expectedOutput[4] = "ERROR Invalid Input. Input should follow a \"<command> <data>\" format";
+		expectedOutput[5] = "DONE 2";
+		expectedOutput[6] = "DISPLAY null";
+		expectedOutput[7] = "DISPLAY 3";
+		expectedOutput[8] = "DISPLAY testing display";
+		expectedOutput[9] = "DISPLAY #hash";
+		expectedOutput[10] = "EDIT null";
+		expectedOutput[11] = "EDIT 3";
+		expectedOutput[12] = "EDIT null";
+		expectedOutput[13] = "REMOVE null";
+		expectedOutput[14] = "REMOVE 3";
+		expectedOutput[15] = "REMOVE hello";
+		expectedOutput[16] = "UNDO null";
+		expectedOutput[17] = "QUIT null";
+		expectedOutput[18] = "HELP null";
+		
+		// run test
+		for (int i=0; i<input.length; i++) {
+			CMD userCMD = p.translateToCMD(input[i]);
+			actualOutput[i] = userCMD.getCommandType().toString();
+			if (userCMD.getData() != null) {
+				actualOutput[i] = actualOutput[i] + " " + userCMD.getData().toString();
+			}
+			else {
+				actualOutput[i] = actualOutput[i] + " null";
+			}
+		}
+		
+		assertArrayEquals(expectedOutput, actualOutput);
+	}
+
+	@Test
+	public void testIsDate() {
+		
+		// initialise arrays to store test cases and answers
+		String[] input = new String[10];
+		String[] actualOutput = new String[10];
+		String[] expectedOutput = new String[10];
+		
+		// initialise expectedOutputs and actualOutputs to true;
+		for (int i=0; i<actualOutput.length; i++) {
+			actualOutput[i] = "true";
+			expectedOutput[i] = "true";
+		}
+		
+		// set test cases
+		input[0] = "1/12/12/12";// false - strArr != 3
+		input[1] = "32/10/2012";// false - day is more than 31
+		input[2] = "0/10/2012";	// false - day is less than 1
+		input[3] = "1/13/2012";	// false - month is more than 12
+		input[4] = "1/12/2222";	// false - year exceeds limit
+		input[5] = "28/02/12";	// false - not a valid format
+		input[6] = "12122012";	// false - not a valid format
+		input[7] = "12May2012";	// false - not a valid format
+		input[8] = "24/01/1999";// true - valid date
+		input[9] = "28/10/2012";// true - valid date
+		
+		// set expectedOutput results
+		// expectedOutput[8] and [9] will be true
+		for (int i=0; i<8; i++) {
+			expectedOutput[i] = "false";
+		}
+		
+		// run test
+		for (int i=0; i<input.length; i++) {
+			if (p.isDate(input[i])) {
+				actualOutput[i] = "true";
+			}
+			else {
+				actualOutput[i] = "false";
+			}
+		}
+
+		assertArrayEquals(expectedOutput, actualOutput);	
+	}
+
+	@Test
+	public void testDetermineCmdEditMode() {
+		
+		// initialise arrays to store test cases and answers
+		String[] input = new String[10];
+		String[] answer = new String[2];
+		String[] actualOutput = new String[10];
+		String[] expectedOutput = new String[10];
+				
+		// set test cases
+		input[0] = "descriptions";	// invalid, returns error
+		input[1] = "due";			// invalid, returns error
+		input[2] = "hasheS";		// invalid, returns error
+		input[3] = "nothing";		// invalid, returns error
+		input[4] = "test";			// invalid, returns error
+		input[5] = "endTIME";		// valid, returns endtime
+		input[6] = "q";				// valid, returns end
+		input[7] = "H";				// valid, returns hash
+		input[8] = "Display";		// valid, returns display
+		input[9] = "desc";			// valid, returns description
+				
+		// set expectedOutput results
+		for (int i=0; i<5; i++) {
+			expectedOutput[i] = "error";
+		}
+		expectedOutput[5] = "endtime";
+		expectedOutput[6] = "end";
+		expectedOutput[7] = "hash";
+		expectedOutput[8] = "display";
+		expectedOutput[9] = "description";
+				
+		// run test
+		for (int i=0; i<input.length; i++) {
+			answer = p.determineCmdEditMode(input[i]);
+			actualOutput[i] = answer[0];	// just catch the first word that is returned
+		}
+		
+		assertArrayEquals(expectedOutput, actualOutput);
+	}
+
+}
