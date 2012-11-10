902a02a741932faf655bb1bf81e7de566aa8ff2e
Ray <chengwm@gmail.com>
2012-11-10 14:59:00 +0800

    Undo for Done command completed. Fixed bug where it doesn't sort the list after undo of done.

---
+++ src/Storage.java
@@ -0,0 +1,322 @@
+// import all classes for now
+import java.util.*;
+import java.util.logging.Logger;
+import java.io.*;
+
+class Storage {
+	// uses Entries class.
+
+	// Variables
+	// Readers, Writers
+	// ArrayList/lists etc for quick access
+	// File for link to doc.
+
+	// Functions
+	// - searches for items with a certain criteria, returns list
+	// - save function: writes into txt file
+	// - load function: loads from txt file
+	// - add/remove function: adding or deleting entries from list
+
+	private static Storage storage;
+	private File activeFile;
+	private File archiveFile;
+	private File activeTextFile;
+	private File archiveTextFile;
+	private Vector<Entry> activeEntries;
+	private Vector<Entry> archiveEntries;
+	private Vector<Entry> displayEntries;
+	private FileWriter fw;
+	private BufferedWriter bw;
+	private static final Logger logger = Logger.getLogger(Control.class.getName());
+
+	//Singleton implementation. Call this to create Storage
+	public static Storage getInstance() {
+		
+		
+		if (storage == null) {
+			storage = new Storage();
+		}
+		return storage;
+	}
+	
+	/*
+	 * Initialise attributes. 
+	 * Checks if activeFile and archiveFile exists.
+	 * Creates them if they do not exist.
+	 * Load activeFile and archiveFile into activeEntries and archiveEntries
+	 */
+	private Storage(){
+		logger.setParent(UI.getLoggingParent());
+		logger.info("Initialising Storage.");
+		
+		activeFile = new File("activeFile.txt");
+		archiveFile = new File("archiveFile.txt");
+		activeTextFile = new File("activeTextFile.txt");
+		archiveTextFile = new File("archiveTextFile.txt");
+		activeEntries = new Vector<Entry>();
+		archiveEntries = new Vector<Entry>();
+		displayEntries = activeEntries;
+		
+		if (activeFile.exists()) {
+			logger.fine("Loading Active Entries.");
+			loadFromFile(activeFile, activeEntries);
+			Collections.sort(activeEntries);
+			logger.fine("Done.");
+		}
+		else{
+			logger.info("No active list found.");
+			try {
+				logger.fine("Creating new list.");
+				activeFile.createNewFile();
+				System.out.println("No active list found. New list created.");
+				logger.fine("Created.");
+			} catch (IOException e) {
+				logger.severe("Unable to Create File.");
+				System.out.println("IOException for activeFile.");
+				System.exit(-1);
+			}
+		}
+
+		if (archiveFile.exists()) {
+			logger.fine("Loading Active Entries.");
+			loadFromFile(archiveFile, archiveEntries);
+			logger.fine("Done.");
+		}
+		else{
+			logger.info("No archive found.");
+			try {
+				logger.fine("Creating new archive.");
+				archiveFile.createNewFile();
+				System.out.println("No archive found. New archive created.");
+				logger.fine("Created.");
+			} catch (IOException e) {
+				logger.severe("Unable to Create File.");
+				System.out.println("IOException for archiveFile.");
+				System.exit(-1);
+			}
+		}
+		
+		
+	}
+
+	private void loadFromFile(File source, Vector<Entry> destination) {
+		// read from file
+		try {
+			FileInputStream newFile = new FileInputStream(source);
+			ObjectInputStream restore = new ObjectInputStream(newFile);
+			Entry entry;
+			while ((entry = (Entry) restore.readObject()) != null) {
+				destination.add(entry);
+			}
+			restore.close();
+		} catch (EOFException eofe) {
+		} catch (ClassNotFoundException cnfe) {
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+			System.out.println("Error loading from file");
+		} 
+
+	}
+	
+	/*
+	 * Save activeEntries and archiveEntries into activeFile and archiveFile
+	 * (RMB TO CALL THIS METHOD BEFORE EXITING PROGRAM!
+	 * ~storage.saveToStorage())
+	 */
+	
+	public void save(boolean activeChanged, boolean archiveChanged){
+		if(activeChanged) saveToFile(activeFile, activeEntries, activeTextFile);
+		if(archiveChanged) saveToFile(archiveFile, archiveEntries, archiveTextFile);
+	}
+	
+	public void saveToFile(File objDest, Vector<Entry> list, File txtDest) {
+
+		// copy entries from ArrayList back to the respective files
+		try {
+			FileOutputStream saveFile = new FileOutputStream(objDest);
+			ObjectOutputStream objWriter = new ObjectOutputStream(saveFile);
+			fw = new FileWriter(txtDest);
+			bw = new BufferedWriter(fw);
+			for (Entry entry : list) {
+				objWriter.writeObject(entry);
+				bw.write(entry + "\n");
+			}
+			objWriter.close();
+		} catch (IOException ioe) {
+			System.out.println("Error writing to file");
+		}
+	}
+
+	/*
+	 * Adds new entry into the activeEntries list. ~storage.addEntry(ENTRY_NAME)
+	 */
+	public void addEntry(Entry entry) {
+		activeEntries.add(entry);
+		Collections.sort(activeEntries);
+	}
+
+	/*
+	 * The entry in the displayEntries that match the index specified by the
+	 * user will be removed from the displayEntries list. Subsequently, update
+	 * activeEntries list to remove the specific entry from storage.
+	 * ~storage.removeEntry(INDEX_OF_ENTRY)
+	 */
+	public Entry removeEntry(int index) {
+		Entry e = displayEntries.get(index-1);
+		displayEntries.remove(index-1);
+		activeEntries.remove(e);
+		return e;
+	}
+	
+	public void removeEntry(Entry entry){
+		displayEntries.remove(entry);
+		activeEntries.remove(entry);		
+	}
+	
+	public Entry removeEntry(String id) {
+		for(Entry e : activeEntries){
+			if(id.equals(e.getID())){
+				activeEntries.remove(e);
+				return e;
+			}
+		}
+		return null;
+	}
+	
+	/*
+	 * This method should only be called after the display function is called.
+	 * From control, the entry to be marked done will be passed to this method
+	 * to be added to the archiveEntries list. Subsequently, update activeEntries
+	 * list to remove the specific entry from storage.
+	 * ~storage.updateCompletedEntry(ENTRY_OBJECT)
+	 */
+	public Entry updateCompletedEntry(int index) {		
+		Entry e = removeEntry(index);
+		archiveEntries.add(e);
+		return e;
+	}
+	
+	public void undoDoneAction(Entry entry){
+		archiveEntries.remove(entry);
+		activeEntries.add(entry);
+		Collections.sort(activeEntries);
+	}
+
+	/*
+	 * Setters and getters methods for printing active/archive/displayEntries
+	 * for (Entry entry : storage.getActiveEntries()) {
+	 * System.out.println("activeList: " + entry); }
+	 */
+	public Vector<Entry> getActiveEntries() {
+		return activeEntries;
+	}
+
+	public void setActiveEntries(Vector<Entry> entries) {
+		this.activeEntries = entries;
+	}
+
+	public Vector<Entry> getArchiveEntries() {
+		return archiveEntries;
+	}
+
+	public void setArchiveEntries(Vector<Entry> entries) {
+		this.archiveEntries = entries;
+	}
+
+	public Vector<Entry> getDisplayEntries() {
+		return displayEntries;
+	}
+
+	public void setDisplayEntries(Vector<Entry> entries) {
+		this.displayEntries = entries;
+	}
+
+	/*
+	 * Different methods for different display commands. Have to decide on the
+	 * return type after that 1. displayAll 2. displayKeyword 3. displayDate (to
+	 * be implemented)
+	 */
+
+	/*
+	 * Method to display all entries. Entries will be copied over to
+	 * displayEntries for printing. displayEntries will be initialized each time
+	 * this method is called. ~storage.displayAll()
+	 */
+//	public Vector<Entry> displayAll() {
+//		displayEntries.clear();
+//		for (Entry entry : activeEntries) {
+//			displayEntries.add(entry);
+//		}
+//		return displayEntries;
+
+		// CODE FOR PRINTING OF DISPLAY ENTRIES
+		// for (Entry entry : storage.getDisplayEntries()) {
+		// System.out.println(entry); }
+//	}
+	
+	public Vector<Entry> display(){
+		displayEntries = activeEntries;
+		return displayEntries;
+	}
+	
+	/*
+	 * Method to display entries by specified keyword. Entries will be copied
+	 * over to displayEntries for printing. displayEntries will be initialized
+	 * each time this method is called. Keyword can be description, hashtag
+	 * "#tagname", venue "@location". ~storage.displayKeyword(KEYWORD_TO_FIND)
+	 */
+	public Vector<Entry> displayKeyword(String keyword) {
+		
+		keyword = keyword.toLowerCase();
+		
+		displayEntries = new Vector<Entry>();
+		for (Entry entry : activeEntries) {
+			if (entry.toString().toLowerCase().contains(keyword)) {
+				displayEntries.add(entry);
+			}
+		}	
+		return displayEntries;	
+		
+	}
+	
+	/*
+	 * Method to display entries by specified keyword. Entries will be copied
+	 * over to displayEntries for printing. displayEntries will be initialized
+	 * each time this method is called. Keyword can be description, hashtag
+	 * "#tagname", venue "@location". ~storage.displayKeyword(KEYWORD_TO_FIND)
+	 */
+	public Vector<Entry> displayIndex(int index) {
+		Vector<Entry> temp = new Vector<Entry>();
+		Entry e = displayEntries.get(index); 
+		temp.add(e);
+		return temp;
+	}
+
+	/*
+	 * Method to display entries by specified date. Entries will be copied over
+	 * to displayEntries for printing. displayEntries will be initialized each
+	 * time this method is called. User to enter date in the form dd/mm/yyyy.
+	 * ~storage.displayDate(dd/mm/yyyy)
+	 */
+	public Vector<Entry> displayDate(String date) {
+		displayEntries.clear();
+		Entry newEntry = new Entry();
+		newEntry.setDateCal(date);
+		for (Entry entry : activeEntries) {
+			if (entry.getDate().equals(newEntry.getDate())) {
+				displayEntries.add(entry);
+			}
+		}
+		return displayEntries;
+	}
+
+	//CLEAR function
+	//removes all entries from activeEntries
+	public void clearActive(){
+		activeEntries = new Vector<Entry>();
+		displayEntries = activeEntries;
+		assert activeEntries.isEmpty();		// assert all entries cleared
+	}
+
+
+}
---
+++ src/UI.java
@@ -0,0 +1,497 @@
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+
+
+public class UI extends JFrame implements ActionListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTextField textField;
+	private JTextArea mainArea;
+	Vector<String> columnNames;
+	private JTable table;
+	private String userInput;
+	
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {	
+		
+		Runnable run = new Runnable(){
+			public void run() {
+				try {
+					UI frame = new UI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		
+		EventQueue.invokeLater(run);
+
+		
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public UI() {
+		//FingerTips Portion
+		initialiseLogger();
+		
+		sc = new Scanner(System.in);
+		editMode = false;
+		cont = true;
+		control = Control.getInstance();
+		
+		logger.info("Initialization Complete.");
+		
+		
+		//END
+		
+		setTitle("FingerTips");
+		
+		setResizable(false);
+		setBackground(Color.WHITE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1140, 550);
+		getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBackground(new Color(240, 240, 240));
+		textField.setBounds(700, 432, 420, 73);
+		textField.setFont(new Font("Consolas", Font.PLAIN, 13));
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		textField.setBorder(new TitledBorder(new LineBorder(new Color(192, 192, 192), 3), "Enter input:", TitledBorder.LEADING, TitledBorder.TOP, new Font("Consolas", Font.PLAIN, 11), new Color(102, 102, 102)));
+		textField.addActionListener(new inputListener());
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(700, 21, 420, 403);
+		getContentPane().add(scrollPane);
+		scrollPane.setBorder(new LineBorder(new Color(139, 0, 139), 2));
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		
+		mainArea = new JTextArea();
+		printWelcomeMSG();
+		scrollPane.setViewportView(mainArea);
+		// set view to stick to the bottom of the message box, but cannot 
+		scrollPane.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener() {  
+			public void adjustmentValueChanged(AdjustmentEvent e) {  
+			e.getAdjustable().setValue(e.getAdjustable().getMaximum());  
+			}});
+		
+		mainArea.setEditable(false);
+		mainArea.setFont(new Font("Consolas", Font.PLAIN, 13));
+		mainArea.setBackground(new Color(204, 193, 218));
+		mainArea.setLineWrap(true);
+		
+		columnNames = new Vector<String>();
+		columnNames.add("ID");
+		columnNames.add("Task");
+		columnNames.add("Start Time");
+		columnNames.add("End Time");
+		columnNames.add("Due Date");
+		columnNames.add("Priority");
+		
+		Vector<Entry> a = control.getStorage().getDisplayEntries();
+		Vector<Vector<String>> data = convertToVV(a);
+		
+		table = new JTable(data, columnNames);
+		table.getTableHeader().setFont(new Font("Consolas", Font.BOLD, 13));
+		table.getTableHeader().setBackground(new Color(75, 172, 198));
+		table.getTableHeader().setForeground(new Color(255, 255, 255));
+	    TableCellRenderer renderer = new TableCellRenderer() {
+	        JLabel label = new JLabel();
+	        @Override
+	        public Component getTableCellRendererComponent(JTable table,
+	                Object value, boolean isSelected, boolean hasFocus,
+	                int row, int column) {
+	            label.setOpaque(true);
+	            label.setFont(new Font("Consolas", Font.PLAIN, 13));
+	            if (value != null) {
+	            	label.setText("" + value);
+	            } else {
+	            	label.setText("");
+	            }
+	            Color alternate = UIManager.getColor("table.alternateRowColor");
+	            if (row % 2 == 1) {
+	                label.setBackground(alternate);
+	            } else {
+	                label.setBackground(new Color(198, 217, 241));
+	            }
+	            return label;
+	        }
+	    };
+	    table.setDefaultRenderer(Object.class, renderer);
+		table.setShowVerticalLines(false);
+		table.setShowHorizontalLines(false);
+		table.setEnabled(false);
+		table.setFont(new Font("Consolas", Font.PLAIN, 13));
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		table.getColumnModel().getColumn(0).setPreferredWidth(20);
+		table.getColumnModel().getColumn(1).setPreferredWidth(290);
+		table.getColumnModel().getColumn(2).setPreferredWidth(80);
+		table.getColumnModel().getColumn(3).setPreferredWidth(80);
+		table.getColumnModel().getColumn(4).setPreferredWidth(90);
+		table.getColumnModel().getColumn(5).setPreferredWidth(89);
+		
+	
+		// automatically resize the columns whenever the data in the table changes
+		table.getModel().addTableModelListener(new TableModelListener() {
+		    public void tableChanged(TableModelEvent e) {
+		        ColumnsAutoSizer.sizeColumnsToFit(table);
+		    }
+		});
+		
+		JScrollPane scrollPane2 = new JScrollPane(table);
+		scrollPane2.setOpaque(true);
+		scrollPane2.setBorder(new LineBorder(new Color(252, 213, 181)));
+		scrollPane2.setBackground(new Color(252, 213, 181));
+		scrollPane2.setBounds(15, 61, 670, 439);
+		scrollPane2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		getContentPane().add(scrollPane2);
+		
+		JLabel lblCurrentList = new JLabel("Task List");
+		lblCurrentList.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCurrentList.setOpaque(true);
+		lblCurrentList.setBackground(new Color(192, 80, 77));
+		lblCurrentList.setForeground(new Color(255, 255, 255));
+		lblCurrentList.setFont(new Font("Consolas", Font.PLAIN, 22));
+		lblCurrentList.setBounds(15, 26, 670, 24);
+		getContentPane().add(lblCurrentList);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(192, 80, 77), 2));
+		panel.setBackground(new Color(252, 213, 181));
+		panel.setBounds(10, 20, 680, 485);
+		getContentPane().add(panel);
+		
+		mainArea.append("\nCommand: ");
+
+	}
+	
+	private Vector<Vector<String>> convertToVV(Vector<Entry> a) {
+		// TODO Auto-generated method stub
+		Vector<Vector<String>> out = new Vector<Vector<String>>();
+		for(int j=0; j<a.size(); j++)
+			out.add(new Vector<String>());
+		
+		int i = 0;
+		
+		for(Entry e : a){
+			Vector<String> v = out.get(i);
+			v.add(i+1+"");
+			v.add(e.getDesc());
+			v.add(e.getStart());
+			v.add(e.getEnd());
+			v.add(e.getDate());
+			v.add(e.getPriority());
+			i++;
+		}
+		
+		return out;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent a) {
+		// TODO Auto-generated method stub
+		userInput = textField.getText();
+		//display_output = control.performAction(userInput);
+	}
+	
+	//FingerTips portion
+	
+	private static final String MSG_WELCOME = "Welcome To FingerTips!\n";
+	private static final String MSG_DEFAULT_ASSISTANCE = "Enter \"help\" for further usage instructions.\n";
+	
+	private static final String SUCCESS_MSG_ADD = "Added.\n";
+	private static final String SUCCESS_MSG_REMOVE = "Removed.\n";
+	private static final String SUCCESS_MSG_DONE = "Entry marked as done and shifted to archive.\n";
+	private static final String SUCCESS_MSG_CLEAR = "All active entries deleted.\n";
+	private static final String SUCCESS_MSG_EXIT = "Goodbye.\n";
+	
+	private static final Logger logger = Logger.getLogger(FingerTips.class.getName());
+	private static final String logFile = "runLog.log";
+	private static final Level handlerLevel = Level.FINE;
+	private static final Level loggerLevel = Level.FINE;
+	
+	private Control control;
+	private Scanner sc;
+	private boolean cont, editMode;
+	
+	class inputListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(!editMode){	
+				
+				userInput = textField.getText();
+				textField.setText(null);
+				mainArea.append(userInput);
+				userInput = userInput.trim();
+				
+				runUserInput(userInput);
+				
+				if(editMode) mainArea.append("\nCommand (Edit Mode): ");
+				else mainArea.append("\nCommand: ");
+			} else {					
+				userInput = textField.getText().trim().toLowerCase();
+				textField.setText(null);
+				mainArea.append(userInput);
+				//call processor
+				String[] response = control.processEditMode(userInput);
+				if(response[0].equals("display")) mainArea.append("\n" +response[1]);
+				else if(response[0].equals("help")) mainArea.append("\n" +helpEditMode());
+				else if(response[0].equals("end")) {
+					editMode = false;
+					control.setEditHolder(null);
+					mainArea.append("\nEdit Mode ended.");
+					mainArea.append("\nCommand: ");
+					return;
+				}
+				else if(response[0].equals("Error")) mainArea.append("\n" +response[1]);
+				
+				mainArea.append("\nCommand (Edit Mode): ");
+			}
+			
+			refreshTable();
+		}
+	}
+	
+	private void initialiseLogger() {
+		try {
+			Handler h = new FileHandler(logFile);
+			h.setFormatter(new SimpleFormatter());
+			h.setLevel(handlerLevel);
+			logger.addHandler(h);
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		logger.setLevel(loggerLevel);
+		logger.info("Logger initialization complete.");
+	}
+	
+	public void runUserInput(String userInput) {
+		CMD actionMSG = control.performAction(userInput);
+		
+		logger.info(actionMSG.toString());
+		
+		followUpAction(actionMSG);
+		
+
+	}
+
+	/**
+	 * 
+	 */
+	private void refreshTable() {
+		Vector<Entry> a = control.getStorage().getDisplayEntries();
+		Vector<Vector<String>> data = convertToVV(a);
+		
+
+		DefaultTableModel dm = new DefaultTableModel(data, columnNames);
+		table.setModel(dm);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		table.getColumnModel().getColumn(0).setPreferredWidth(20);
+		table.getColumnModel().getColumn(1).setPreferredWidth(290);
+		table.getColumnModel().getColumn(2).setPreferredWidth(80);
+		table.getColumnModel().getColumn(3).setPreferredWidth(80);
+		table.getColumnModel().getColumn(4).setPreferredWidth(90);
+		table.getColumnModel().getColumn(5).setPreferredWidth(89);
+		dm.fireTableDataChanged();
+	}
+	
+	private void followUpAction(CMD actionMSG) {
+		switch(actionMSG.getCommandType()){
+		case ADD: 		add(actionMSG); 		break;
+		case REMOVE: 	remove(actionMSG);		break;
+		case UNDO: 		undo(actionMSG);		break;
+		case DISPLAY:	display(actionMSG);		break;
+		case EDIT:		edit();					break;
+		case DONE: 		done(actionMSG);		break;
+		case HELP: 		mainArea.append("\n" + help());
+												break;
+		case QUIT: 		quit();					break;
+		case CLEAR:		clear();				break;
+		case ERROR:		error(actionMSG);		break;
+		case REDO:		redo(actionMSG);		break;
+		default: undo(actionMSG);
+		}
+	}
+	
+	//Add action
+		private void add(CMD actionMSG) {
+			if(actionMSG.getData()==null){
+				mainArea.append("\n" +"Please enter a description for your task:");
+				String description = textField.getText().trim();
+				textField.setText(null);
+				description = "add \"".concat(description);
+				control.performAction(description);
+			}
+			
+			control.setEditHolder(null);
+			Collections.sort(control.getStorage().getActiveEntries());
+			mainArea.append("\n" +SUCCESS_MSG_ADD);
+	
+		}
+		
+		//Edit Mode
+		private void edit() {
+			editMode = true;
+			mainArea.append("\n" +"Entry: ");
+			mainArea.append(control.processEditMode("display")[1]);
+
+			mainArea.append("\n" +"Enter the field you wish to modify, and the new data to replace with.");
+			mainArea.append("\n" +"Type \"end\" to exit edit mode and \"help\" for futher assistance.");
+		}
+
+		private void display(CMD actionMSG) {
+			Vector<Entry> print = (Vector<Entry>) actionMSG.getData();
+			if(print.isEmpty()){
+				mainArea.append("\n" +"There is nothing to print.");
+			}
+		}
+
+		private void remove(CMD actionMSG) {
+			
+			if(actionMSG.getData() == null){
+				mainArea.append("\n" +"Which entry do you want to remove?");
+				int rmvIndex;
+				try{
+					System.out.print("Index: ");
+					rmvIndex = Integer.parseInt(textField.getText());
+					String newInstruction = "remove ".concat(Integer.toString(rmvIndex));
+					control.performAction(newInstruction);
+					mainArea.append("\n");
+					
+				} catch(InputMismatchException e){
+					mainArea.append("\n" +"Invalid input. Action aborted.");
+
+				}
+
+				sc.nextLine();
+			}
+			mainArea.append("\n" +SUCCESS_MSG_REMOVE);
+		}
+
+		//Follow up for DONE
+			private void done(CMD actionMSG) {
+				mainArea.append("\n" +SUCCESS_MSG_DONE);
+			}
+		
+		//Prints action taken.
+		private void undo(CMD actionMSG) {
+			
+			mainArea.append("\n" +actionMSG.getData());
+		}
+		
+		private void redo(CMD actionMSG) {
+			
+			mainArea.append("\n" +actionMSG.getData());
+		}
+		
+		//Prints SUCCESS MSG for Clear
+		private void clear() {
+			mainArea.append("\n" +SUCCESS_MSG_CLEAR);
+		}
+
+		//Ending MSG
+		private void quit() {
+			cont = false;
+			mainArea.append("\n" +SUCCESS_MSG_EXIT);
+			 try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.exit(0);
+		}
+		
+		
+		//Print ERROR msg
+		//Assumes:	CMD.data is a String
+		private void error(CMD actionMSG) {
+			mainArea.append("\n" +actionMSG.getData());
+		}
+		
+		//Welcome Message
+		private void printWelcomeMSG() {
+			mainArea.append(MSG_WELCOME);
+			mainArea.append(MSG_DEFAULT_ASSISTANCE);
+		}
+		
+		// default help message
+		private static String help() {
+			return ("add <data>:\t   add an entry with related dates,\t\t\t   description, priority etc.\n") +
+					("\t\t   prefix @ indicates venue, prefix #\t\t\t   indicates a hashtag.\n") +
+					("\nremove <number>:   remove the selected entry for the\t\t\t   active list.\n") +
+					("\nedit <number>:\t   enters edit mode for selected entry.\n") +
+					("\nundo:\t\t   reverses the previous action.\n") +
+					("\ndisplay <keyword>: generates a list of entries\t\t\t\t   fulfilling the search criteria.\n") +
+					("\ndone <number>:\t   marks an entry as completed.\n") +
+					("\nclear:\t\t   deletes all entries permanently\t\t\t   (use with caution!).\n") +
+					("\nquit:\t\t   terminates the program.\n");
+		}
+
+		//editMode help
+		private static String helpEditMode() {
+			return ("\nEnter a field followed by the new data it should be\nreplaced with.\n") +
+					("desc:\t  edit description\n") +
+					("ddate:\t  edit due date\n") +
+					("display:  shows data in the current node\n") +
+					("priority: edit priority\n") +
+					("hash #:\t  edit hash tags\n") +
+					("st:\t  edit start time\n") +
+					("et:\t  edit end time\n") +
+					("venue @:  edit venue\n");
+		}
+		
+		//for logger initialization use
+		public static Logger getLoggingParent(){
+			return logger;
+		}
+}
+
+
